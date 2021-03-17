0de7a4b21ac1aade8224fdaca5baf5cb5947acd0
NilsonKr <nuotrok444@gmail.com>
2021-03-16 22:30:14 -0500

    :tada: [INIT] Basic Configuration

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.(css|s[ac]ss)$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(svg|png|jpg|webp)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.html$/,
+				use: 'html-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: 'index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].[contenthash].css',
+		}),
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		historyApiFallback: true,
+	},
+};
