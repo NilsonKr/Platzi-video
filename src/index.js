254e02924ca7bcd49f9713f7d0cf2ffcc0f93b6f
NilsonKr <nuotrok444@gmail.com>
2021-04-12 17:51:11 -0500

    :sparkles: [ADD] Installing Redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
+
 import '@styles/styles.scss';
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(
+	<Provider>
+		<App />
+	</Provider>,
+	document.querySelector('#app')
+);
