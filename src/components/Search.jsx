b134ba33f2b6a7de8a469a2215778eb695ba53aa
NilsonKr <nuotrok444@gmail.com>
2021-04-16 18:39:00 -0500

    :sparkles: [ADD] useFilter items hook

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { connect } from 'react-redux';
 import '@styles/components/Search.scss';
 
-const Search = () => {
+function useFilter(items) {
+	const [query, setQuery] = useState('');
+	const [filteredItems, setFilteredItems] = useState(items);
+
+	useMemo(() => {
+		const newItems = items.filter(item => {
+			return item.title.toLowerCase().includes(query.toLowerCase()); //Filter Items
+		});
+
+		if (newItems.length !== filteredItems.length) {
+			//Set New Items
+			setFilteredItems(newItems);
+		}
+	}, [query]);
+
+	return [query, setQuery, filteredItems];
+}
+
+const Search = props => {
+	const [query, setQuery, filteredItems] = useFilter(props.items);
+
 	return (
 		<section className='main'>
 			<h2 className='main__title'>¿Qué quieres ver hoy?</h2>
-			<input type='text' className='input' placeholder='Buscar...' />
+			<input
+				type='text'
+				className='input'
+				placeholder='Search...'
+				onChange={e => setQuery(e.target.value)}
+			/>
 		</section>
 	);
 };
 
-export default Search;
+const mapStateToProps = state => ({
+	items: [...state.trends, ...state.originals],
+});
+
+export default connect(mapStateToProps)(Search);
