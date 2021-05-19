2f3f07d90466864260a6e86fe961b3d1b0f6ec85
NilsonKr <nuotrok444@gmail.com>
2021-05-19 18:26:16 -0500

    :tada: [INIT] Dependencies updated for ssr

--- src/context/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Context = React.createContext(null);
-
-const ContextProvider = props => {
-	return <Context.Provider value={{ hola: 'Hola' }}>{props.children}</Context.Provider>;
-};
-
-export default ContextProvider;
