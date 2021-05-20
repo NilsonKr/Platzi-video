440d8a0ead22574ee7b81f5572001eb765575583
NilsonKr <nuotrok444@gmail.com>
2021-05-20 18:44:17 -0500

    :art: [ADD] Serving Statics

--- src/server/server.js
+++ src/server/server.js
@@ -18,25 +18,27 @@ if (config.ENV === 'development') {
 	app.use(hotMiddleware(compiler));
 }
 
-app.get('*', (req, res) => {
-	res.setHeader('Content-Type', 'text/html');
-	res.send(`
-		<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8" />
-			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<link rel="icon" type="image/png" href="./favicon.ico" />
-			<title>React Video</title>
-		</head>
-		<body>
-			<div id="app"></div>
-			<script src="assets/bundle.js" type="text/javascript"></script>
-		</body>
-	</html>
-`);
-});
+// app.get('*', (req, res) => {
+// 	res.setHeader('Content-Type', 'text/html');
+// 	res.send(`
+// 		<!DOCTYPE html>
+// 	<html lang="en">
+// 		<head>
+// 			<meta charset="UTF-8" />
+// 			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+// 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+// 			<link rel="icon" type="image/png" href="./favicon.ico" />
+// 			<title>React Video</title>
+// 		</head>
+// 		<body>
+// 			<div id="app"></div>
+// 			<script src="assets/bundle.js" type="text/javascript"></script>
+// 		</body>
+// 	</html>
+// `);
+// });
+
+app.use('*', express.static('public'));
 
 app.listen(config.PORT, err => {
 	if (err) return console.error(err);
