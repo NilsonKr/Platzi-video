f64f5d64b889539dfeeabc6f35325f9e57da81f0
NilsonKr <nuotrok444@gmail.com>
2021-05-20 18:33:26 -0500

    :sparkles: [ADD] Serving react

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,41 @@
 const config = require('./config/index');
+const webpack = require('webpack');
 
 const express = require('express');
 const app = express();
 
+if (config.ENV === 'development') {
+	console.log('Development Configuration');
+
+	const webpackConfig = require('../../webpack.config.dev');
+	const devMiddleware = require('webpack-dev-middleware');
+	const hotMiddleware = require('webpack-hot-middleware');
+
+	const compiler = webpack(webpackConfig);
+	const serverConfig = { serverSideRender: true };
+
+	app.use(devMiddleware(compiler, serverConfig));
+	app.use(hotMiddleware(compiler));
+}
+
 app.get('*', (req, res) => {
-	res.json({ hello: 'Server Side Rendering' });
+	res.setHeader('Content-Type', 'text/html');
+	res.send(`
+		<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8" />
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<link rel="icon" type="image/png" href="./favicon.ico" />
+			<title>React Video</title>
+		</head>
+		<body>
+			<div id="app"></div>
+			<script src="assets/bundle.js" type="text/javascript"></script>
+		</body>
+	</html>
+`);
 });
 
 app.listen(config.PORT, err => {
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,15 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: [
+		'./src/frontend/index.js',
+		'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
+	],
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.[contenthash].js',
+		filename: 'assets/bundle.js',
 		publicPath: '/',
 		assetModuleFilename: 'assets/[hash][ext]',
 	},
@@ -13,11 +17,11 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx'],
 		alias: {
-			'@assets': path.resolve(__dirname, './src/assets/'),
-			'@components': path.resolve(__dirname, './src/components/'),
-			'@pages': path.resolve(__dirname, './src/pages/'),
-			'@styles': path.resolve(__dirname, './src/styles/'),
-			'@hooks': path.resolve(__dirname, './src/hooks/'),
+			'@assets': path.resolve(__dirname, './src/frontend/assets/'),
+			'@components': path.resolve(__dirname, './src/frontend/components/'),
+			'@pages': path.resolve(__dirname, './src/frontend/pages/'),
+			'@styles': path.resolve(__dirname, './src/frontend/styles/'),
+			'@hooks': path.resolve(__dirname, './src/frontend/hooks/'),
 		},
 	},
 	module: {
@@ -42,15 +46,16 @@ module.exports = {
 		],
 	},
 	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			filename: 'index.html',
-		}),
+		new webpack.HotModuleReplacementPlugin(),
+		// new HtmlWebpackPlugin({
+		// 	template: './public/index.html',
+		// 	filename: 'index.html',
+		// }),
 	],
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		compress: true,
-		historyApiFallback: true,
-		open: true,
-	},
+	// devServer: {
+	// 	contentBase: path.join(__dirname, 'dist'),
+	// 	compress: true,
+	// 	historyApiFallback: true,
+	// 	open: true,
+	// },
 };
