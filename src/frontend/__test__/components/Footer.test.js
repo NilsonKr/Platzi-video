4de2e116ee0b4565e31970fafb36e6084e285b63
NilsonKr <nuotrok444@gmail.com>
2021-06-13 03:17:41 -0500

    :camera_flash: [ADD] Footer Snapshots

--- src/frontend/__test__/components/Footer.test.js
+++ src/frontend/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -11,5 +12,11 @@ describe('<Footer />', () => {
 
 	test('Footer have 3 anchors', () => {
 		expect(footerMounted.find('a')).toHaveLength(3);
+	});
+
+	test('Footer SnapShot', () => {
+		const footerSnap = create(<Footer />);
+
+		expect(footerSnap.toJSON()).toMatchSnapshot();
 	});
 });
